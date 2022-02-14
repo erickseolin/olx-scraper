4c5ddbb93b278306ac6a6d8da960b4cb6f4463a6
Erick Seolin <erickseolin@gmail.com>
2022-02-13 23:29:35 -0300

    refactor scrape function

--- scrape.py
+++ scrape.py
@@ -23,6 +23,56 @@ MONTHS = {
 }
 
 
+def scrape_ad(ad_element):
+    """Scrape a single ad from an ad html element."""
+    ad_link = ad_element.a
+    if ad_link is None:
+        return None
+    ad_link_url = ad_link.attrs['href']
+    photos_div, data_div = ad_link.div.contents[:2]
+    image_url = photos_div.find('img').attrs['src']
+    title = data_div.find('h2').get_text()
+    other_data = [span.get_text() for span in data_div.find_all('span')]
+
+    ad_obj = Ad(
+        title=title,
+        link=ad_link_url,
+        img=image_url,
+        info=other_data.pop(0),
+        value=other_data.pop(0)
+    )
+
+    # discart previous ad value
+    if other_data[0].startswith('R$'):
+        other_data.pop(0)
+
+    publication_day = other_data.pop(0).lower()
+    if publication_day == 'hoje':
+        publication_day = datetime.today()
+    elif publication_day == 'ontem':
+        publication_day = datetime.today() - timedelta(days=1)
+    else:
+        day, month_str = publication_day.split(' ')
+        publication_day = datetime(
+            year=datetime.today().year,
+            month=MONTHS[month_str],
+            day=int(day)
+        )
+
+    hour, minutes = other_data.pop(0).split(':')
+    publication_datetime = publication_day.replace(
+        hour=int(hour),
+        minute=int(minutes)
+    )
+
+    ad_obj.date = publication_datetime
+    ad_obj.location = other_data.pop(0)
+    if other_data:
+        ad_obj.vendor_type = other_data.pop(0)
+
+    return ad_obj
+
+
 def scrape(url):
     """Scrape a OLX ad list page extracting data from each ad"""
     response = requests.get(
@@ -40,53 +90,10 @@ def scrape(url):
         soup = BeautifulSoup(response.text, 'html.parser')
         ads = soup.find('ul', {'id': 'ad-list'})
         scraped_ads = []
-        for ad in ads.contents:
-            ad = ad.a
-            if ad is None:
-                continue
-            ad_link = ad.attrs['href']
-            photos_div, data_div = ad.div.contents[:2]
-            image_url = photos_div.find('img').attrs['src']
-            description = data_div.find('h2').get_text()
-            other_data = [span.get_text()
-                          for span in data_div.find_all('span')]
-
-            ad_obj = Ad(
-                title=description,
-                link=ad_link,
-                img=image_url,
-                info=other_data.pop(0),
-                value=other_data.pop(0)
-            )
-
-            # discart previous value
-            if other_data[0].startswith('R$'):
-                other_data.pop(0)
-
-            publication_day = other_data.pop(0).lower()
-            if publication_day == 'hoje':
-                publication_day = datetime.today()
-            elif publication_day == 'ontem':
-                publication_day = datetime.today() - timedelta(days=1)
-            else:
-                day, month_str = publication_day.split(' ')
-                publication_day = datetime(
-                    year=datetime.today().year,
-                    month=MONTHS[month_str],
-                    day=int(day)
-                )
-
-            hour, minutes = other_data.pop(0).split(':')
-            publication_datetime = publication_day.replace(
-                hour=int(hour),
-                minute=int(minutes)
-            )
-
-            ad_obj.date = publication_datetime
-            ad_obj.location = other_data.pop(0)
-            if other_data:
-                ad_obj.vendor_type = other_data.pop(0)
-            scraped_ads.append(ad_obj)
+        for ad_element in ads.contents:
+            ad_obj = scrape_ad(ad_element)
+            if ad_obj is not None:
+                scraped_ads.append(ad_obj)
 
         with open('result.json', 'w', encoding='utf-8') as output_fp:
             json.dump(
